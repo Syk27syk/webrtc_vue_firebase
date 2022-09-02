e9ec490fb6f371536000f8c5fd1d944effbe9fdd
shiyunkhoo <shiyun.khoo@gmail.com>
2022-09-02 17:01:22 +0800

    firebase retrieve data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <p>Hello {{ user }}! </p>
   </div>
 </template>
 
@@ -18,10 +19,28 @@
 // @ is an alias to /src
 // eslint-disable-next-line
 import HelloWorld from "@/components/HelloWorld.vue";
+// eslint-disable-next-line
+import db from "../db.js";
 
 export default {
   // eslint-disable-next-line
   name: "HomeView",
+  // eslint-disable-next-line
+  data: function() {
+    return {
+      user: null,
+    };
+  },
+  mounted() {
+    // eslint-disable-next-line
+    db.collection('users')
+      // eslint-disable-next-line
+      .doc('AHFJ4wUrnhgh5mpddw74')
+      .get()
+      .then((snapshot) => {
+        this.user = snapshot.data().name;
+      });
+  },
   components: {
     // eslint-disable-next-line
     HelloWorld,
