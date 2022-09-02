affd503864483b90c0f58b93353db4126cd0c2f8
shiyunkhoo <shiyun.khoo@gmail.com>
2022-09-02 16:03:08 +0800

    bootstrap and navigation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
+    <!--
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
+    -->
     <router-view />
   </div>
 </template>
 
+<style lang="scss">
+$primary: #5f2882;
+// eslint-disable-next-line prettier/prettier
+@import "node_modules/bootstrap/scss/bootstrap";
+</style>
+
+<!--
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -30,3 +39,4 @@ nav {
   }
 }
 </style>
+-->
---
+++ src/components/Navigation.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable -->
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link>
+    </nav>
+  </div>
+</template>
